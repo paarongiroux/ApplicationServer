2703f0b2eaf92d8bf3e04e5467d340562a1aa16b
Aaron <pag87@nau.edu>
2020-04-12 21:05:43 -0700

    implementation of satellite skeleton code

---
+++ appserver/satellite/Satellite.java
@@ -0,0 +1,217 @@
+package appserver.satellite;
+
+import appserver.job.Job;
+import appserver.comm.ConnectivityInfo;
+import appserver.job.UnknownToolException;
+import appserver.comm.Message;
+import static appserver.comm.MessageTypes.JOB_REQUEST;
+import static appserver.comm.MessageTypes.REGISTER_SATELLITE;
+import appserver.job.Tool;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Hashtable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import utils.PropertyHandler;
+
+/**
+ * Class [Satellite] Instances of this class represent computing nodes that execute jobs by
+ * calling the callback method of tool a implementation, loading the tool's code dynamically over a network
+ * or locally from the cache, if a tool got executed before.
+ *
+ * @author Dr.-Ing. Wolf-Dieter Otte
+ */
+public class Satellite extends Thread {
+
+    private ConnectivityInfo satelliteInfo = new ConnectivityInfo();
+    private ConnectivityInfo serverInfo = new ConnectivityInfo();
+    private HTTPClassLoader classLoader = null;
+    private Hashtable<String, Tool> toolsCache = null;
+
+    public Satellite(String satellitePropertiesFile, String classLoaderPropertiesFile, String serverPropertiesFile) {
+        // read this satellite's properties and populate satelliteInfo object,
+        // which later on will be sent to the server
+        try
+        {
+            PropertyHandler satProps = new PropertyHandler(satellitePropertiesFile);
+            satelliteInfo.setName(satProps.getProperty("NAME"));
+            satelliteInfo.setPort(Integer.parseInt(satProps.getProperty("PORT")));
+            System.out.println("[Satellite] successfully set up satellite properties.");
+        }
+        catch(IOException e)
+        {
+            System.err.println(e);
+        }
+        
+        
+        // read properties of the application server and populate serverInfo object
+        // other than satellites, the as doesn't have a human-readable name, so leave it out
+        try
+        {
+            PropertyHandler appServerProps = new PropertyHandler(serverPropertiesFile);
+            serverInfo.setHost(appServerProps.getProperty("HOST"));
+            serverInfo.setPort(Integer.parseInt(appServerProps.getProperty("PORT")));
+            System.out.println("[Satellite] successfully set up app server properties.");
+        }
+        catch(IOException e)
+        {
+            System.err.println(e);
+        }
+        
+        // read properties of the code server and create class loader
+        // -------------------
+        try
+        {
+            PropertyHandler classLoaderProps = new PropertyHandler(classLoaderPropertiesFile);
+            String host = classLoaderProps.getProperty("HOST");
+            int port = Integer.parseInt(classLoaderProps.getProperty("PORT"));
+            classLoader = new HTTPClassLoader(host, port);
+            System.out.println("[Satellite] successfully set up class loader.");
+            // doesn't currently do anything with the docroot
+        }
+        catch(IOException e)
+        {
+            System.err.println(e);
+        }
+
+        
+        // create tools cache
+        // -------------------
+        toolsCache = new Hashtable<String, Tool>();
+        
+    }
+
+    @Override
+    public void run() {
+
+        // register this satellite with the SatelliteManager on the server
+        // ---------------------------------------------------------------
+        // ...
+        
+        
+        // create server socket
+        // ---------------------------------------------------------------
+        try
+        {
+            ServerSocket serverSocket = new ServerSocket(satelliteInfo.getPort());
+            System.out.println("[Satellite] successfully set up ServerSocket.");
+            // start taking job requests in a server loop
+            // ---------------------------------------------------------------
+            while (true)
+            {
+                Socket socket = serverSocket.accept();
+                (new SatelliteThread(socket, this)).start();
+            }
+        }
+        catch(IOException e)
+        {
+            System.err.println(e);
+        }   
+    }
+
+    // inner helper class that is instanciated in above server loop and processes single job requests
+    private class SatelliteThread extends Thread {
+
+        Satellite satellite = null;
+        Socket jobRequest = null;
+        ObjectInputStream readFromNet = null;
+        ObjectOutputStream writeToNet = null;
+        Message message = null;
+
+        SatelliteThread(Socket jobRequest, Satellite satellite) {
+            this.jobRequest = jobRequest;
+            this.satellite = satellite;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("[SatelliteThread] starting new thread...");
+            // setting up object streams
+            try
+            {
+                writeToNet = new ObjectOutputStream(jobRequest.getOutputStream());
+                readFromNet = new ObjectInputStream(jobRequest.getInputStream());
+            }
+            catch(IOException e)
+            {
+                System.err.println(e);
+            }  
+            
+            
+            // reading message
+            try
+            {
+                message = (Message) readFromNet.readObject();
+            }
+            catch(ClassNotFoundException | IOException e)
+            {
+                System.err.println(e);
+            }  
+            
+            switch (message.getType()) {
+                case JOB_REQUEST:
+                    // processing job request
+                    Job job = (Job) message.getContent();
+                    try
+                    {
+                        // lookup the tool name
+                        Tool tool = getToolObject(job.getToolName());
+                        // run the tool
+                        Object result = tool.go(job.getParameters());
+                        
+                        // send the result back
+                        writeToNet.writeObject(result);
+                        
+                    }
+                    catch (Exception e)
+                    {
+                        System.err.println(e);
+                    }
+                    
+                    break;
+
+                default:
+                    System.err.println("[SatelliteThread.run] Warning: Message type not implemented");
+            }
+        }
+    }
+
+    /**
+     * Aux method to get a tool object, given the fully qualified class string
+     * If the tool has been used before, it is returned immediately out of the cache,
+     * otherwise it is loaded dynamically
+     */
+    public Tool getToolObject(String toolClassString) throws UnknownToolException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+        Tool toolObject = null;
+
+        // try and get the tool from the toolsCache
+        if ((toolObject = toolsCache.get(toolClassString)) == null)
+        {
+            // if the tool wasn't in the cache, load it with the classLoader
+            Class toolClass = classLoader.loadClass(toolClassString);
+            // create a new Tool instance
+            toolObject = (Tool) toolClass.newInstance();
+            // put the tool into the toolsCache for future re-use
+            toolsCache.put(toolClassString, toolObject);
+        }
+        else
+        {
+            // tool was already in cache. print to console.
+            System.out.println("Tool: \"" + toolClassString + "\" already in Cache");
+        }
+        // return the Tool object
+        return toolObject;
+    }
+
+    public static void main(String[] args) {
+        // start the satellite
+        Satellite satellite = new Satellite(args[0], args[1], args[2]);
+        satellite.run();
+    }
+}
